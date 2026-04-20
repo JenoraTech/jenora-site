986cf6219bc16f441406f7a89256ad385bad42a5
JenoraTech <jenoratech@gmail.com>
2026-04-20 07:58:53 +0100

    seo: exclude admin routes from sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.jenoratech.com.ng',
   generateRobotsTxt: true,
+  // Excludes the admin dashboard and all its sub-pages from the sitemap
+  exclude: ['/admin', '/admin/*'], 
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/admin', // Explicitly tells bots not to crawl the admin area
+      },
+    ],
+  },
 }
